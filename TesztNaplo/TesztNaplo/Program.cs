d6ceffd6a4bbf1ac37849409576e4c209997bccf
Plesz Gábor <gabor.plesz@gmail.com>
2017-02-07 11:28:31 +0100

    XML alapbeállítások

---
+++ TesztNaplo/TesztNaplo/Program.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesztNaplo
+{
+    class Program
+    {
+        private static readonly ILog logger = LogManager.GetLogger("TesztNaplo.Program");
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+        }
+    }
+}
