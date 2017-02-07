7e6d39c81bc1cda0388dd9aae9b38c6bbe0a67b8
Plesz Gábor <gabor.plesz@gmail.com>
2017-02-07 12:31:28 +0100

    Automatikus tesztnaplóküldés

--- TesztNaplo/TesztNaplo/Program.cs
+++ TesztNaplo/TesztNaplo/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TesztNaplo
@@ -15,7 +16,40 @@ namespace TesztNaplo
         {
             log4net.Config.XmlConfigurator.Configure();
 
-            logger.Debug("Elindult a programunk");
+            //naplóírás 
+            var r = new Random();
+
+            Console.WriteLine("Megállításhoz egy gombot kell nyomni...");
+            while (!Console.KeyAvailable)
+            {
+                var val = r.Next(100);
+
+                if (val<60)
+                {
+                    logger.Debug("Ez egy debug");
+                    Thread.Sleep(100);
+                }
+                else if (val < 80)
+                {
+                    logger.Info("Ez egy info");
+                    Thread.Sleep(200);
+                }
+                else if (val < 90)
+                {
+                    logger.Warn("Warning");
+                    Thread.Sleep(300);
+                }
+                else if (val < 97)
+                {
+                    logger.Error("Error");
+                    Thread.Sleep(400);
+                }
+                else 
+                {
+                    logger.Fatal("Fatal");
+                    Thread.Sleep(400);
+                }
+            }
 
             log4net.LogManager.Shutdown();
 
